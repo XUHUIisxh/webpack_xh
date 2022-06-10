7ab2cede3b88b032357a261c6090b1a0dcbb8d7e
xh <xh980103@163.com>
2022-06-10 09:26:21 +0800

    ✨ webpack 初体验

---
+++ 01webpack初体验/src/index.js
@@ -0,0 +1,8 @@
+import { sum } from './js/math.js'
+import { dataFormate } from './js/formate'
+const data = require('./js/commonjs')
+
+
+console.log(sum(1, 2))
+console.log(dataFormate())
+console.log(data())
